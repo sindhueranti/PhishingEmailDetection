c286f0103019cc40aad696bbc8c677db08901afc
sindhueranti <global>
2023-02-28 14:39:29 -0800

    Initial Checkin

---
+++ src/main/java/com/nyit/springboot/PhishingEmailDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.nyit.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingEmailDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishingEmailDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyit/springboot/PhishingEmailDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyit.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingEmailDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
